2b894117056f59baadd353985e80aef739c63590
zhangjunqi <1974097603@qq.com>
2020-01-08 13:11:18 +0800

    zjq

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React,{Component} from 'react'
 import {Route,Switch,Redirect} from 'react-router-dom'
 import Profile from './pages/Profile/Profile'
-import Login from './pages/Login/Login'
-import Test from './pages/test'
 import Ershoufang from './pages/ershoufang/ershoufang'
 import Home from './pages/home/home'
 import Detail from './pages/detail/detail'
@@ -17,12 +15,9 @@ export default class App extends Component{
         <Header></Header>
         <Switch>
           <Route path="/home" component={Home}/>
-          {/* <Route path="/login" component={Login}/> */}
           <Route path="/detail" component={Detail}/>
           <Route path="/ershoufang" component={Ershoufang}/>
-          <Route path="/login" component={Login}/>
           <Route path="/profile/:index" component={Profile}/>
-          <Route path="/test" component={Test}/>
           <Route path="/newhouse" component={NewHouse}/>
           <Redirect to="/home"/>
         </Switch>
